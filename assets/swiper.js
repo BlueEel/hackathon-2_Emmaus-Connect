1540fae73b61a024387f8697dfee7b3fe047a69d
Arthur Gaychet <arthur.gaychet81@gmail.com>
2023-06-29 14:50:50 +0200

    carrousel swiper et fixtures

---
+++ assets/swiper.js
@@ -0,0 +1,19 @@
+import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle'
+import './styles/swiper.scss'
+
+window.Swiper = Swiper;
+
+const swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 15,
+  freeMode: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
